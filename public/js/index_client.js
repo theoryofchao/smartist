b5e9c08bc4f4ea6fc0fc88ceb112392e1b9838e7
drakaven <drakaven@hotmail.com>
2017-02-01 18:10:50 -0500

    added google snipperScanner to catgorize

--- public/js/index_client.js
+++ public/js/index_client.js
@@ -1,4 +1,4 @@
-$(document).ready(function (){
+$(document).ready(function () {
 
   //Test Data
   var testElements = [
@@ -10,14 +10,14 @@ $(document).ready(function (){
 
   //functions for getting and rendering elements
   var getElements = function (elements) {
-    renderElements(elements);
-    /*$.get(("/todo"), function (data, status) {
+    $.get(("/todo"), function (data, status) {
+      console.log(data);
       renderElements(data);
-    });*/
+    });
   };
 
   var createElement = function (element) {
-    let newArticle = $('<article class="tweet"></article>');
+    var newArticle = $('<article class="tweet"></article>');
     newArticle[0].innerHTML = `
     <article>
         <span>${element.name}</span> <span>${element.category}</span>
@@ -32,6 +32,49 @@ $(document).ready(function (){
       todoContainer.append(createElement(elements[element]));
     }
   };
+
+  $('#todoButton').on('click', function () {
+    getCategory($('#todoText').val());
+  });
+
+var getCategory = function (item) {
+  var searchString = `https://www.googleapis.com/customsearch/v1?q=${item}&cx=009727429418526168478%3Agmz1zju4st8&num=10&key=AIzaSyBaDb1wlcW9WXDPpRzPanZr58lhtl1UnIo`;
+  $.get((searchString), function (data, result) {
+    let category = "";
+    for (dataObj in data.items) {
+      //console.log(data.items[dataObj].snippet);
+      category = snippetScanner(data.items[dataObj].snippet);
+      if (category !== "") {
+        break;
+      }
+    }
+    if (category === "") {category = "product"};
+    console.log(category, "end")
+  });
+
+
+  var snippetScanner = function (snippet) {
+    var dictionary =
+      {
+        restaurant: "restaurant",
+        movie: "movie",
+        film: "movie",
+        book: "book",
+        novel: "book"
+
+      };
+
+    var category = "";
+    for (value in dictionary) {
+      if (snippet.indexOf(dictionary[value]) !== -1) {
+        category = dictionary[value];
+        break;
+      }
+    }
+    return category;
+  }
+}
+
   //initial page load
   getElements(testElements);
 });
