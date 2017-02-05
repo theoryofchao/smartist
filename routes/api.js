7303dce61b49490792a758534a20c7335a699e05
matthew jenkins <drakaven@hotmail.com>
2017-02-05 23:16:04 +0000

    Amazon

--- routes/api.js
+++ routes/api.js
@@ -18,13 +18,22 @@ const amazonTemplate = function (results){
           <p>${review}</p>`;
 };
 
+const youtubeTemplate = function (videoId){
+  console.log(videoId);
+  return  `<iframe id="ytplayer" allowfullscreen="allowfullscreen" type="text/html" width="320" height="180"
+            src="https://www.youtube.com/embed/${videoId}"
+               frameborder="0"></iframe>`
+};
 
 
 //http://webservices.amazon.com/scratchpad/index.html
 router.post('/amazon', (request, response) => {
+  let searchIndex = "";
+  if (request.body.category === 'Product') {searchIndex = 'All'} else {searchIndex = request.body.category};
+
   client.itemSearch({
     Keywords: request.body.title,
-    searchIndex: request.body.category,
+    searchIndex: searchIndex,
     responseGroup: 'ItemIds'
 
   }).then(function (results) {
@@ -40,11 +49,31 @@ router.post('/amazon', (request, response) => {
 
     }).then(function (results) {
       //console.log(amazonTemplate(results));
-      response.status(200).send(amazonTemplate(results));
-      //response.status(200).json(results);
+      let retValue =  (amazonTemplate(results));
+      if (request.body.category === 'Movies') {
+        let youTube = reqApi.get('http://127.0.0.1:3000/api/youtube/' + request.body.title + "movie%20trailer" , function (error, res, body) {
+         return response.status(200).send(retValue + res.body);
+        });
+      } else {
+        return response.status(200).send(retValue);
+      }
     }).catch(function (err) {
       console.log(err);
     });
+  });
+});
+
+
+
+router.get('/youtube/:id', (request, response) => {
+  options = {
+    url: `https://www.googleapis.com/youtube/v3/search?part=id&q=${request.params.id}&key=AIzaSyBaDb1wlcW9WXDPpRzPanZr58lhtl1UnIo`
+  };
+  //send request
+  reqApi(options, function (error, response, body) {
+  }).on('complete' , function(result){
+    let parsed = JSON.parse(result.body).items[0].id.videoId;
+    response.status(200).send(youtubeTemplate(parsed));
   });
 });
 
